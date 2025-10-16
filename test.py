09a3342a0b9c798347d994e0a7725e341f51f07f
Jonas-Buliga <buliga.jonas@seznam.cz>
2025-10-16 14:48:13 +0000

    xxx

---
+++ test.py
@@ -0,0 +1,24 @@
+def je_prvocislo(cislo):
+    """
+    Funkce overi, zda zadane cislo je nebo neni prvocislo a vrati True nebo False
+
+    Prvocislo je takove cislo vetsi nez 1, ktere neni delitelne zadnym jinym cislem jenom 1 a samo sebou.
+    """
+    if cislo < 2:
+        return False
+    for i in range(2, int(cislo ** 0.5) + 1):
+        if cislo % i == 0:
+            return False
+    return True
+
+def vrat_prvocisla(maximum):
+    """
+    Funkce spocita vsechna prvocisla v rozsahu 1 az maximum a vrati je jako seznam.
+    """
+    maximum = int(maximum)
+    return [i for i in range(2, maximum + 1) if je_prvocislo(i)]
+
+if __name__ == "__main__":
+    cislo = input("Zadej maximum: ")
+    prvocisla = vrat_prvocisla(cislo)
+    print(prvocisla)
