7c2b408917211690c9030d052b2009e5736419c1
Jonas-Buliga <buliga.jonas@seznam.cz>
2025-10-23 06:19:31 +0000

    ukol3.opraveno

--- third.py
+++ third.py
@@ -8,10 +8,9 @@ def je_prvocislo(cislo):
     return True
 
 def vrat_prvocisla(maximum):
-    """
-    Funkce spocita vsechna prvocisla v rozsahu 1 az maximum a vrati je jako seznam.
-    """
-    return [2,3,5]
+    
+    maximum = int(maximum)
+    return [i for i in range(2, maximum + 1) if je_prvocislo(i)]
 
 
 if __name__ == "__main__":
